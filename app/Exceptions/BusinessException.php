a756eefba9f312baffd529c596eef491d0ec2c0f
Minh Hien <hien.nguyenhominh@gmail.com>
2026-06-18 09:52:03 +0000

    chore(exception): add application exceptions

---
+++ app/Exceptions/BusinessException.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+
+class BusinessException extends Exception
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
---
+++ app/Exceptions/MockApiException.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+
+class MockApiException extends Exception
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
